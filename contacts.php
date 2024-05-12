07beccee72db3b8750521fef31ba66f8ee1349b4
Li18ko <lizakorneeva2004@gmail.com>
2024-05-12 17:12:13 +0300

    готова страница контаков

--- contacts.php
+++ contacts.php
@@ -26,18 +26,62 @@
                     ул. Вишневая, д. 30, офис 10</p>
                 </div>
                 <div class="contacts_up_left_bottom">
-                    <p>Наш адрес <br>
-                    Юридический: 390048 Россия, г. Рязань, <br>
-                    ул. Зубковой, д. 21, кв. 44 <br>
-                    Фактический: 390044 Россия, г. Рязань, <br>
-                    ул. Вишневая, д. 30, офис 10</p>
+                    <div class="contacts_up_left_bottom_left">
+                        <p>Наши контакты </p>
+                        <p>+7 (4912) 99-43-34</p>
+                        <p>+7 (4912) 51-61-56 </p>
+                        <p>zakaz@geomtsk.ru </p>
+                        <p>geometry062@gmail.com </p>
+                        <p>www.(название сайта)</p>
+                    </div>
+                    <div class="contacts_up_left_bottom_right">
+                        <p>Наши реквизиты </p>
+                        <p>ООО “ГЕОМЕТРИЯ”</p>
+                        <p>ОГРН 1176234003945</p>
+                        <p>ИНН 6234166179</p>
+                    </div>
                 </div>
             </div>
             <div class="contacts_up_right">
-            
+                <img src="image/image 62.png">
+                <img src="image/image 3.png">
+                <img src="image/image 17.png">
             </div>  
         </div>
-
+      </div>
+      <div class="jobOpenings">
+        <h1>Вакансии</h1>
+        <div class="jobOpenings_Data">
+            <div class="jobOpenings_Data_left">
+                <div class="jobOpenings_Data_left_up">
+                    <p>Инженерно-технологический персонал:</p>
+                    <li>производитель общестроительных работ</li>
+                    <li>инженер проектировщик (КЖ, КМ)</li>
+                    <li>специалист сварочного производства</li>
+                </div>
+                <div class="jobOpenings_Data_left_bottom">
+                    <p>Рабочие строительных специальностей:</p>
+                    <li>сварщики технологических трубопроводов НАКС</li>
+                    <li>сварщики резервуаров НАКС</li>
+                    <li>сварщики металлоконструкций</li>
+                    <li>монтажники металлоконструкций</li>
+                    <li>бетонщики монолитчики</li>
+                </div>
+            </div>
+            <div class="jobOpenings_Data_right">
+                <div class="jobOpenings_Data_right_up">
+                    <p>Отправить Ваше резюме можно на почту zakaz@geomtsk.ru <br>
+                    За дополнительной информацией по открытым вакансиям обращайтесь по телефону <br>
+                    +7 (4912) 99-43-34</p>
+                </div>
+                <div class="jobOpenings_Data_right_bottom">
+                    <p>Мы приветствуем появление в своих рядах специалистов, чья энергия и <br>
+                    профессиональные знания будут соответствовать в достижении компании <br>
+                    “ГЕОМЕТРИЯ” своих амбициозных целей. Если выхотите стать участником команды <br>
+                    профессионалов “ГЕОМЕТРИЯ”, вы можете отправить в кадровую службу свое резюме</p>
+                </div>
+            </div>
+        </div>
       </div>
     </main>
     <?php include 'footer.html'; ?>
