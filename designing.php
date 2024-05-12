f287d89737aa4622dcd5a1e2667b49d7c809d9e7
Li18ko <lizakorneeva2004@gmail.com>
2024-05-12 17:40:57 +0300

    готов раздел проектирование

---
+++ designing.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <title>Проектирование</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <?php include 'header.html'; ?>
+    <main>
+        <div class="designing">
+            <div class="designing_up">
+                <img src="image/image 13.png">
+            </div>
+            <div class="designing_bottom">
+                <div class="designing_bottom_left">
+                    <p>Перед началом строительных работ необходимо провести проектирование, которое <br>
+                    является критической фазой процесса. Качественная разработка проекта определяет <br>
+                    внешний и внутренний облик здания, его безопасность, прочность и долговечность, а <br>
+                    также позволяет заранее оценить стоимость материалов и строительства.<br>
+                    <br>
+                    Наша компания специализируется на проектировании зданий и сооружений <br>
+                    разнообразной сложности и назначения. Мы учитываем все факторы, включая местные <br>
+                    особенности, законодательные нормы, а также окружающую застройку. Наша команда <br>
+                    опытных специалистов гарантирует качественную реализацию проекта, учитывая все <br>
+                    пожелания и потребности клиента.<br>
+                    <br>
+                    Мы стремимся создавать уникальные и функциональные объекты, отвечающие <br>
+                    современным стандартам и ожиданиям заказчика. Наша цель - обеспечить <br>
+                    оптимальное соотношение цены и качества, чтобы наши клиенты получали самые <br>
+                    удовлетворительные результаты.<br>
+                    <br>
+                    Мы нацелены на долгосрочное сотрудничество и стремимся к доверительным <br>
+                    отношениям с нашими клиентами. Мы предоставляем всестороннюю поддержку и <br>
+                    консультации на всех этапах проектирования и строительства, чтобы убедиться, что <br>
+                    результат соответствует ожиданиям клиента.</p>
+                </div>
+                <div class="designing_bottom_right">
+                    <div class="designing_bottom_right_left">
+                        <img src="image/image 59.png">
+                    </div>
+                    <<div class="designing_bottom_right_right">
+                        <img src="image/image 79.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php include 'footer.html'; ?>
+  </body>
+
+</html>
